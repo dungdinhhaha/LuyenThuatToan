3b609dee9a5148363eb4728e44bccded2eedfba4
dungdinhhaha <dungdinh542004@gmail.com>
2025-12-16 09:35:45 +0700

    Xóa không up các folder untitled2, algorithms_upload, bai10

--- bai10/src/oop/XeBonBanh.java
+++
@@ -1,9 +0,0 @@
-
-package oop;
-
-public abstract class XeBonBanh extends Xe {
-    public XeBonBanh() {}
-    public XeBonBanh(String mauSon, double trongLuong) {
-        super(mauSon, trongLuong);
-    }
-}
--- bai10/src/toantu.java
+++
@@ -1,9 +0,0 @@
-public class toantu {
-
-    public static void main(String[] args) {
-        int d = 1;
-        d = d++ + ++d - d++;
-        System.out.println(d);
-    }
-    }
-
